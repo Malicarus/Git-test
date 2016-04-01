84aa4c25c63467b7ebd1699b3650a5c7ddc011bb
Tyler Archer <tylsarch@ut.utm.edu>
2016-03-31 20:27:43 -0500

    Adding first files (main.cpp) to the project.

---
+++ main.cpp
@@ -0,0 +1,13 @@
+//Name:Tyler Archer
+//Date: 31 March 2016
+//File: main.cpp
+//Description: Simple hello world to test git
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  cout << "Hello World!" << endl;
+}
